fcf0eb3e7a0c9153168d4fe3717d458fbfdde6a1
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-03-16 20:24:16 -0400

    feat: added skeleton navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="flex flex-wrap justify-between items center mx-auto max-w-screen-xl">
+          <div>
+            <p>INSERT LOGO HERE</p>
+          </div>
+        <div className="navbar-links">
+          <p>Projects</p>
+          <p>About</p>
+        </div>
+        </div>
+      </nav>
+    </>
+  );
+}
