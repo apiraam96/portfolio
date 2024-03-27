18d24a494ac83d9c2195b086c58988ae672c2fc7
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-03-26 23:53:00 -0400

    style: changed intro words

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@
 
 import Image from "next/image";
 import Navbar from "./components/Navbar";
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import {TextPlugin} from 'gsap/TextPlugin'
+import { TextPlugin } from "gsap/TextPlugin";
 import { RoughEase } from "gsap/EasePack";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+gsap.registerPlugin(TextPlugin);
 
 export default function Home() {
-  const words = ["Apiraam.", "A Scientist.", "A Developer."];
+  const words = [" Apiraam.", " a Scientist.", " a Developer."];
+  const text = useRef<HTMLInputElement>(null);
 
   useGSAP(() => {
     let cursor = gsap.to(".cursor", {
@@ -27,15 +30,15 @@ export default function Home() {
         delay: 0.5,
         ease: "power4.inOut",
       })
-      .from('.hi', {duration: 1, y: "7vw", ease: "power3.out"})
+      .from(".hi", { duration: 1, y: "7vw", ease: "power3.out", onComplete: () => masterTimeline.play() })
       .to(".box", { duration: 1, height: "7vw", ease: "elastic.out" });
 
-      let masterTimeline = gsap.timeline();
-      words.forEach(word => {
-        let timeline = gsap.timeline()
-        timeline.to('.text', {duration: 1, text: word})
-        masterTimeline.add(timeline)
-      })
+    let masterTimeline = gsap.timeline({ repeat: -1, delay: 1 }).pause();
+    words.forEach((word) => {
+      let timeline = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 });
+      timeline.to(".text", { duration: 1, text: word });
+      masterTimeline.add(timeline);
+    });
   });
 
   return (
