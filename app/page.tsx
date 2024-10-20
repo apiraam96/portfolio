71420b95a4659ac55817ff37531aa290e98eeafd
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-10-19 21:01:33 -0400

    test: testing removal of gif for prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Image from "next/image";
-import Landing from "./components/Landing";
+import React, { lazy } from "react";
+// import Landing from "./components/Landing";
+import { Suspense } from "react";
 import { FloatingNav } from "./components/ui/NavBar";
-import { FaHome } from "react-icons/fa";
-import Grid from "./components/ui/Grid";
-import { useScroll, useTransform } from "framer-motion";
-import { LampContainer, LampDemo } from "./components/ui/lamp";
-import { HeroHighlight } from "./components/ui/HeroHighlight";
-import { Highlight } from "./components/ui/HeroHighlight";
-import Projects from "./components/Projects";
+// import Grid from "./components/ui/Grid";
+// import Projects from "./components/Projects";
 import { navItems } from "@/data";
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
+
+const Landing = lazy(() => import("./components/Landing"));
+const Grid = lazy(() => import("./components/ui/Grid"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
 
 export default function Home() {
   return (
@@ -23,10 +23,18 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Landing />
-        <Grid />
-        <Projects />
-        <Contact />
+        <Suspense>
+          <Landing />
+        </Suspense>
+        <Suspense>
+          <Grid />
+        </Suspense>
+        <Suspense>
+          <Projects />
+        </Suspense>
+        <Suspense>
+          <Contact />
+        </Suspense>
       </div>
     </main>
   );
